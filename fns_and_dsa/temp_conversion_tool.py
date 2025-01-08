f06e0c61955918c0d4268fbedfd61b53c0fa5222
lohochris <159076124+lohochris@users.noreply.github.com>
2025-01-08 15:52:47 +0100

    Add files via upload

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,9 @@
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+
+# Placeholder functions to satisfy the script structure
+def convert_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+
+def convert_to_fahrenheit(celsius):
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
